b936271612a2dac58e55b8cabc62bfa485f30adc
RayGITspace <rjj.laakso@saunalahti.fi>
2025-11-05 16:02:09 +0200

    error controller fixed to ES6

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,16 +1,20 @@
 "use strict";
+import { httpStatus } from "http-status-codes";
 
-const httpStatus = require("http-status-codes");
-
-exports.pageNotFoundError = (req, res) => {
+const.pageNotFoundError = (req, res) => {
   let errorCode = httpStatus.NOT_FOUND;
   res.status(errorCode);
   res.render("error");
 };
 
-exports.internalServerError = (error, req, res, next) => {
+const.internalServerError = (error, req, res, next) => {
   let errorCode = httpStatus.INTERNAL_SERVER_ERROR;
   console.log(`ERROR occurred: ${error.stack}`);
   res.status(errorCode);
   res.send(`${errorCode} | Sorry, our application is taking a nap!`);
+};
+
+export const errorController = {
+  pageNotFoundError,
+  internalServerError,
 };
